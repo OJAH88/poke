f97d241c1d8eb60c934e98389deac4a9955cede4
OJAH88 <jharlan628@gmail.com>
2021-12-08 12:58:48 -0500

    jah 12:58pm

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -3,10 +3,11 @@ import { useState } from "react"
 import React, { useParams } from "react-router"
 import useFetch from "../../useFetch"
 
-export default function Login() {
+export default function Login( {onLogin}) {
     const [username, setUsername] = useState('')
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const [passwordConfirmation, setPasswordConfirmation] = useState('')
     const [isLoading, setIsLoading] = useState(false);
 
     function handleSubmit(e) {
@@ -31,14 +32,14 @@ export default function Login() {
                     <span className="loginDesc">Connect with like minded individuals and poke'em!</span>
                 </div>
                 <div className="loginRight">
-                    <form onSubmit={handleSubmit}
                     <div className="loginBox">
-                        <input placeholder="Email" type="email" className="loginInput"value={email}
-        onChange={(e) => setUsername(e.target.value)} />
-                        <input placeholder="Password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="loginInput" />
+                    <form onSubmit={handleSubmit}>
+                        <input placeholder="Email" className="loginInput" type="email" required value={email} onChange={(e) => setEmail(e.target.value)} />
+                        <input placeholder="Password" className="loginInput" type="password" required value={password} onChange={(e) => setPassword(e.target.value)} />
                         <button className="loginButton" type="submit">Log In</button>
                         <span className="loginForgot">Forgot Password?</span>
                         <button className="loginRegisterButton">Create a New Account</button>
+                        </form>
                     </div>
                 </div>
             </div>
