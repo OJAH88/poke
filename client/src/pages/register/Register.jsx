3c591ac35fc12f93b4f699028293b0143c73d4bf
Amy Posten <amyposten85@gmail.com>
2021-12-08 12:33:00 -0500

    Updated node modules

--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -1,60 +1,88 @@
-import "./register.css"
-import { useState } from "react"
-import React, { useParams } from "react-router"
-import useFetch from "../../useFetch"
-
-
+import "./register.css";
+import { useState } from "react";
+import React, { useParams } from "react-router";
+import useFetch from "../../useFetch";
 
 const Register = () => {
-    const [username, setUsername] = useState('')
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [passwordAgain, setPasswordAgain] = useState('')
-    const [isLoading, setIsLoading] = useState(false);
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordAgain, setPasswordAgain] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
-
-const handleSubmit = (e) => {
-    e.preventDefault()
-    const newUser = { username, email, password, passwordAgain}
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newUser = { username, email, password, passwordAgain };
 
     setIsLoading(true);
 
     setTimeout(() => {
-        fetch('http://localhost:4000/users', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(newUser)
-        }).then(() => {
-            console.log("New User Added")
-            setIsLoading(false)
-        })
-    },600)}
+      fetch("http://localhost:4000/users", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(newUser),
+      }).then(() => {
+        console.log("New User Added");
+        setIsLoading(false);
+      });
+    }, 600);
+  };
 
-
-
-
-    return(
-        <div className="login">
-            <div className="loginWrapper">
-                <div className="loginLeft">
-                    <h3 className="loginLogo">Pokebook</h3>
-                    <span className="loginDesc">Connect with like minded individuals and poke'em!</span>
-                </div>
-                <div className="loginRight">
-                    <div className="loginBox">
-                        <form onSubmit={handleSubmit}>
-                        <input placeholder="Username" className="loginInput" type="text" required value={username} onChange={(e) => setUsername(e.target.value)}/> <br />
-                        <input placeholder="Email" className="loginInput" type="email" required value={email} onChange={(e) => setEmail(e.target.value)}/> <br />
-                        <input placeholder="Password" className="loginInput" type="password" required value={password} onChange={(e) => setPassword(e.target.value)} /> <br />
-                        <input placeholder="Confirm Password" className="loginInput" type="password" required value={passwordAgain} onChange={(e) => setPasswordAgain(e.target.value)}/> <br />
-                        <button className="loginButton">Sign Up</button>
-                        <button className="loginRegisterButton">Log into Account</button>
-                        </form>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="login">
+      <div className="loginWrapper">
+        <div className="loginLeft">
+          <h3 className="loginLogo">Pokebook</h3>
+          <span className="loginDesc">
+            Connect with like minded individuals and poke'em!
+          </span>
         </div>
-    )
-    
-    }
-    export default Register
+        <div className="loginRight">
+          <div className="loginBox">
+            <form onSubmit={handleSubmit}>
+              <input
+                placeholder="Username"
+                className="loginInput"
+                type="text"
+                required
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />{" "}
+              <br />
+              <input
+                placeholder="Email"
+                className="loginInput"
+                type="email"
+                required
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />{" "}
+              <br />
+              <input
+                placeholder="Password"
+                className="loginInput"
+                type="password"
+                required
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />{" "}
+              <br />
+              <input
+                placeholder="Confirm Password"
+                className="loginInput"
+                type="password"
+                required
+                value={passwordAgain}
+                onChange={(e) => setPasswordAgain(e.target.value)}
+              />{" "}
+              <br />
+              <button className="loginButton">Sign Up</button>
+              <button className="loginRegisterButton">Log into Account</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Register;
